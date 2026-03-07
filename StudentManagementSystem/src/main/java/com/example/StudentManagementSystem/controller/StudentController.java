52fc174dd88a4e8dbb617ef60bb1363962a3a14e
Shah Zaman <engrshahzaman09@gmail.com>
2026-03-08 04:39:16 +0500

    Spring Boot Student Management System

---
+++ StudentManagementSystem/src/main/java/com/example/StudentManagementSystem/controller/StudentController.java
@@ -0,0 +1,38 @@
+package com.example.StudentManagementSystem.controller;
+
+
+import com.example.StudentManagementSystem.model.Student;
+import com.example.StudentManagementSystem.service.StudentService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+
+public class StudentController {
+    @Autowired
+    private StudentService studentService;
+
+    @GetMapping
+    public List<Student> getAllStudents(){
+        return studentService.getAllStudents();
+    }
+    @PostMapping
+    public Student addStudent(@RequestBody Student student){
+        return studentService.addStudent(student);
+    }
+
+    @DeleteMapping("/{id}")
+        public void deleteStudent(@PathVariable Long id){
+            studentService.deleteStudent(id);
+
+    }
+
+    @PutMapping("/{id}")
+        public Student updateStudent(@PathVariable Long id, @RequestBody Student student){
+        return studentService.updateStudent(id, student);
+    }
+
+}
